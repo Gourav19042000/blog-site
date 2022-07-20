30385df00c23882c0e3fb2883dd2264e24dcd391
Gourav19042000 <59428586+Gourav19042000@users.noreply.github.com>
2022-07-20 09:36:01 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+const Article = require('./models/article')
+const methodOverride=require('method-override')
+mongoose.connect('mongodb://localhost/blog', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+
+app.use(methodOverride('_method'))
+
+const articleRouter = require('./routes/articles')
+app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: false }))
+app.use('/articles', articleRouter)
+
+
+app.get('/', async (req, res) => {
+ 
+  const articles = await Article.find().sort({ createdAt: 'desc' })
+  res.render('articles/index', { articles: articles })
+})
+app.listen(5000)
